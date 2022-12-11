a9f50fff1dc9fdb3b264ba031cba5fed75195cc0
kahosan <15116355657l@gamil.com>
2022-12-11 23:10:34 +0800

    feal: select a serverUrl to prefix

--- src/components/LocalAlias.vue
+++ src/components/LocalAlias.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
-import { NButton, NInput, NInputGroup, NTag } from 'naive-ui'
+import { NButton, NCheckbox, NCheckboxGroup, NInput, NInputGroup, NTag } from 'naive-ui'
 import { ref } from 'vue'
 
 import { useLocalAlias } from '../composables/use-local-alias'
+import { useServerUrl } from '../composables/use-server-url'
 
-const alias = useLocalAlias(document.URL)
+const aliasStorage = useLocalAlias()
 const inputText = ref('')
+
+const serverUrlStorage = useServerUrl()
+const selectServerUrl = ref(null)
+
+const tipIsShow = ref(false)
+if (selectServerUrl.value)
+  tipIsShow.value = false
+
+const addServerUrl = () => {
+  if (!selectServerUrl.value || !inputText.value) {
+    tipIsShow.value = true
+    return
+  }
+  if (inputText.value) {
+    const alias = selectServerUrl.value + inputText.value
+    const suffix = alias.endsWith('/') ? '' : '/'
+
+    aliasStorage.value = alias + suffix
+    tipIsShow.value = false
+  }
+}
 </script>
 
 <template>
@@ -13,26 +35,27 @@ const inputText = ref('')
     <NInput
       v-model:value="inputText"
       size="small"
-      placeholder="目录别名..."
-      @keyup="alias = inputText"
-    >
-      <template #prefix>
-        <span>本地目录别名</span>
-      </template>
-    </NInput>
-    <NButton
-      type="tertiary"
-      size="small"
-      @click="alias = inputText"
-    >
+      placeholder="本地目录别名..."
+      @keyup.enter="addServerUrl"
+    />
+    <NButton type="tertiary" size="small" @click="addServerUrl">
       添加
     </NButton>
   </NInputGroup>
-  <NTag v-if="alias" closable @close="alias = null">
-    {{ alias }}
+  <NTag v-if="aliasStorage" closable @close="aliasStorage = null">
+    {{ aliasStorage }}
   </NTag>
+  <NCheckboxGroup v-model:value="selectServerUrl">
+    <NCheckbox v-for="serverUrl in serverUrlStorage" :key="serverUrl" :value="serverUrl">
+      {{ serverUrl }}
+    </NCheckbox>
+  </NCheckboxGroup>
   <div class="tip">
-    比如这个页面上动漫的中文名是「孤独摇滚!」, 而你的目录里的名称是 「孤独摇滚」没有！，就可以把 孤独摇滚 填进去
+    比如这个页面上动漫的中文名是「孤独摇滚!」, 而你的目录里的名称是 「孤独摇滚」没有！，就可以把 孤独摇滚 填进去。
+    需要指定一下是哪个目录。
+  </div>
+  <div v-show="tipIsShow" class="tip-2">
+    {{ inputText ? '请选择一个服务器' : '请输入一个本地目录别名' }}
   </div>
 </template>
 
@@ -40,5 +63,13 @@ const inputText = ref('')
 .tip {
   margin-top: 0.25rem;
   line-height: 1.4;
+  font-size: 12px;
+}
+
+.tip-2 {
+  margin-top: 0.25rem;
+  line-height: 1.4;
+  font-size: 12px;
+  color: red
 }
 </style>
