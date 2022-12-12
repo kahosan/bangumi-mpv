fa05e4e5e0e0e9a065fc289d371779bcef303515
kahosan <15116355657l@gmail.com>
2022-12-12 15:16:01 +0800

    feat: support iina

--- src/components/MpvMenu.vue
+++ src/components/MpvMenu.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import { NButton } from 'naive-ui'
+import { useIINA } from '../composables/use-iina'
 import LocalAlias from './LocalAlias.vue'
 import ServerUrl from './ServerUrl.vue'
+
+const iina = useIINA()
 </script>
 
 <template>
   <ServerUrl />
   <LocalAlias />
+  <NButton size="small" @click="iina = !iina">
+    使用 IINA 播放
+  </NButton>
 </template>
 
 <style scoped>
