50f2e57ca3966e8b167e3c74ffb068820d299792
kahosan <15116355657l@gmail.com>
2022-12-12 13:09:15 +0800

    feat: handler sub folder

--- src/composables/use-get-anime-file-url.ts
+++ src/composables/use-get-anime-file-url.ts
@@ -1,3 +1,5 @@
+import { matchSubFolderReg, matchVideoReg } from '../util/reg'
+
 import { useAjax } from './use-ajax'
 import { useGetAnimeList } from './use-get-anime-list'
 import { useGetAnimeName } from './use-get-anime-name'
@@ -9,10 +11,9 @@ export async function useGetAnimePathUrl(alias?: string | null) {
   let animePathUrl = ''
   let error = true
 
-  const url = await getFileUrl(alias || '', '9')
-  console.log(url)
-
-  //   return alias
+  // 如果有别名，直接返回别名
+  if (alias !== undefined && alias !== null)
+    return alias
 
   const animeList = await useGetAnimeList(serverUrls)
 
@@ -37,20 +38,41 @@ export async function useGetAnimePathUrl(alias?: string | null) {
   }
 }
 
-export async function getFileUrl(baseUrl: string, epId: string) {
-  const url = new URL(epId, baseUrl).href
-  const dom = (await useAjax({ url })).responseText
-  let movieUrl: string | undefined
+export async function useGetAnimeFileUrl(baseUrl: string, epId: string) {
+  try {
+    const url = new URL(`${epId}/`, baseUrl).href
+    const dom = (await useAjax({ url })).responseText
+    let movieUrl: string | undefined
 
-  movieUrl = dom.match(/(?<=<a href=").*(?=\.*(?<!\/|.ass)">)/)?.[0]
-  if (movieUrl)
-    return new URL(movieUrl[0], baseUrl).href
+    movieUrl = dom.match(matchVideoReg)?.[0]
+    if (movieUrl)
+      return new URL(movieUrl, url).href
 
-  movieUrl = await handleSubFolder(url)
-  return movieUrl || 'asd'
+    movieUrl = await handleSubFolder(url)
+    return movieUrl
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
 
-async function handleSubFolder(url: string) {
-  const reg = /(?<=<a href="(?!..\/)).*(?=">)/
-  return ''
+// 递归往子文件夹里找视频文件，返回找到的第一个视频文件的 url
+async function handleSubFolder(url: string): Promise<string | undefined> {
+  try {
+    const dom = (await useAjax({ url })).responseText
+    const movieUrl = dom.match(matchVideoReg)?.[0]
+    if (movieUrl)
+      return new URL(movieUrl, url).href
+
+    const subFolderList = dom.match(matchSubFolderReg)
+    if (subFolderList) {
+      for (const subFolder of subFolderList) {
+        if (subFolder)
+          return await handleSubFolder(new URL(subFolder, url).href)
+      }
+    }
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
---
+++ src/util/reg.ts
@@ -0,0 +1,2 @@
+export const matchVideoReg = /(?<=href=")([^"]*(mp4|mkv|mov|flv))/
+export const matchSubFolderReg = /(?<=href=")(?!(\/|\.\/|\.\.\/))([^"]*\/)/g
