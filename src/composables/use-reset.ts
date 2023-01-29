aad0b914f25dd19675ef3ed3ed8921561cf05817
kahosan <15116355657l@gmail.com>
2023-01-29 17:14:00 +0800

    feat: reset error

--- src/composables/use-reset.ts
+++ src/composables/use-reset.ts
@@ -1,8 +1,10 @@
 export function useReset(fn: (event: HTMLAnchorElement) => void) {
   const episodeUlEle = document.querySelector('.prg_list')
 
-  if (!episodeUlEle)
+  if (!episodeUlEle) {
+    console.error('剧集 html 标签重置事件失败')
     return
+  }
 
   for (const episodeLiEle of episodeUlEle.children) {
     const epATag = episodeLiEle.children[0] as HTMLAnchorElement
